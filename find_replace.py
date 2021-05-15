6cac57296bb967147c21311045945898a3139d5b
Avery Khoo <averykhoo@gmail.com>
2021-05-15 09:42:35 +0800

    fix levenshtein lookup

--- find_replace.py
+++ find_replace.py
@@ -36,6 +36,9 @@ class Match:
                  ):
         """
         match result (similar to re.Match, but currently using token indices because of how tokenization works)
+        todo: normalize to char index
+        todo: use a frozen dataclass
+        todo: just refer to original string once we have the char indices, for compat
 
         :param start: index of start TOKEN (not char)
         :param end: index after end token
@@ -316,6 +319,7 @@ class Trie(object):
             self.pop(key)
 
     def items(self):
+        # todo: special case for empty str?
         _path = []
         _stack = [(self.root, sorted(self.root.keys(), reverse=True))]
         while _stack:
@@ -530,100 +534,67 @@ class Trie(object):
             self[sequence] = replacement
         return self
 
-    def levenshtein_lookup(self, word: str, distance: int):
+    def levenshtein_lookup(self,
+                           word: str,
+                           distance: int,
+                           insertion_cost: Union[int, float] = 1,
+                           deletion_cost: Union[int, float] = 1,
+                           substitution_cost: Union[int, float] = 1,
+                           ) -> Generator[str, Any, None]:
         """
-        lazy and probably not correct levenshtein lookup
-        but better than nothing
-        tests seem to indicate that it functions correctly though
-        and idk why
-        todo: fix
-
-        probably better to go through all the words one by one
-        but stop and backtrack once it passes the distance threshold
-        also then the results will be in lexicographically sorted order, which is nice
-
-        need path to store where we are and the distances at each node
-        maybe use 2 lists
-        and a stack to store where we need to go
-        each next step has to contain the current node and next step
-
+        levenshtein / edit distance based approximate string lookup
+        todo: maybe try implementing as damerau-levenshtein (ie. with transposition)
+        todo: maybe allow returning values not just keys?
+        todo: special case for empty str?
+        todo: special case for distance = 0
         """
         assert list(self.tokenizer('test-test test')) == list('test-test test'), "shouldn't use a tokenizer"
         assert distance >= 0
         assert isinstance(word, str)
         assert len(word) > 0
-        #
-        # _path = []
-        # _distances = []
-        # _stack = [(self.root, sorted(self.root.keys(), reverse=True), 0)]
-        # while _stack:
-        #     head, keys = _stack.pop(-1)
-        #     if keys:
-        #         key = keys.pop(-1)
-        #         _stack.append((head, keys))
-        #         head = head[key]
-        #         _path.append(key)
-        #         if head.REPLACEMENT is not _NOTHING:
-        #             yield self.detokenizer(_path)  # , head.REPLACEMENT
-        #         _stack.append((head, sorted(head.keys(), reverse=True)))
-        #     elif _path:
-        #         _path.pop(-1)
-        #     else:
-        #         assert not _stack
-        states: List[Tuple[Tuple, Trie.Node, int]] = [((), self.root, 0)]
+        assert insertion_cost >= 0
+        assert deletion_cost >= 0
+        assert substitution_cost >= 0
 
-        def insertion():
-            """
-            doesn't perform multiple insertions one after another
-            which means the implementation may be incorrect
-            """
-            nonlocal states
-            states, _prev_states = [], states
-            for _prev, _state, _distance in _prev_states:
-                states.append((_prev, _state, _distance))
-                if _distance + 1 <= distance:
-                    for _key, _next_state in _state.items():
-                        states.append(((_prev, _key), _next_state, _distance + 1))  # insertion
+        _path = []
+        _dp_table = [range(len(word) + 1)]
+        _stack = [(self.root, sorted(self.root.keys(), reverse=True))]
 
-        insertion()
+        while _stack:
+            head, keys = _stack.pop(-1)
+            if keys:
+                key = keys.pop(-1)
+                _stack.append((head, keys))
 
-        def dedupe():
-            """
-            should just use a dict from the start, instead of using a list and having to dedupe
-            but the dedupe might also not be totally correct
-            """
-            nonlocal states
-            states, _prev_states = [], states
-            temp = dict()
-            for _prev, _state, _distance in _prev_states:
-                if _prev not in temp:
-                    temp[_prev] = (_distance, _state)
-                elif temp[_prev][0] > _distance:
-                    temp[_prev] = (_distance, _state)
-            for _prev, (_distance, _state) in temp.items():
-                states.append((_prev, _state, _distance))
+                assert len(_dp_table) > 0
+                next_head = head[key]
+                next_row = [len(_dp_table)]
 
-        for char in word:
-            states, prev_states = [], states
-            for prev, state, current_distance in prev_states:
-                if current_distance + 1 <= distance:
-                    states.append((prev, state, current_distance + 1))  # deletion
-                    for key, next_state in state.items():
-                        states.append(((prev, key), next_state, current_distance + (key != char)))  # substitution
-                elif char in state:
-                    states.append(((prev, char), state[char], current_distance))
-            insertion()
-            dedupe()
+                for idx_2, char_2 in enumerate(word):
+                    # [idx_2 + 1] instead of j since _dp_table[-1] and current_row are one character longer than word
+                    insertions = _dp_table[-1][idx_2 + 1] + insertion_cost
+                    deletions = next_row[idx_2] + deletion_cost
+                    if key == char_2:
+                        substitutions = _dp_table[-1][idx_2]
+                    else:
+                        substitutions = _dp_table[-1][idx_2] + substitution_cost
+                    next_row.append(min(insertions, deletions, substitutions))
 
-        for string, state, _dist in states:
-            assert _dist <= distance
-            if state.REPLACEMENT is not _NOTHING:
-                tmp = []
-                while string:
-                    tmp.append(string[-1])
-                    string = string[0]
-                tmp.reverse()
-                yield ''.join(tmp)
+                # early exit?
+                if min(next_row) <= distance:
+                    _path.append(key)
+                    _dp_table.append(next_row)
+                    _stack.append((next_head, sorted(next_head.keys(), reverse=True)))
+                    if next_row[-1] <= distance and next_head.REPLACEMENT is not _NOTHING:
+                        yield self.detokenizer(_path)  # , next_head.REPLACEMENT
+
+            elif _path:
+                _path.pop(-1)
+                _dp_table.pop(-1)
+
+            else:
+                assert not _stack
+                assert len(_dp_table) == 1
 
     def _yield_tokens(self,
                       file_path: Union[str, os.PathLike],
